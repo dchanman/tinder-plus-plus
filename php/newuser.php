f32106154e83f81bf3e1858117728c2d8744cbce
peterysc <peterysc@gmail.com>
2016-02-27 15:16:17 -0800

    newUser features to be discussed

--- php/newuser.php
+++ php/newuser.php
@@ -10,5 +10,12 @@
 		<?php echo 'Location <input type="text" name="signup_location"><br>'; ?>
 		<?php echo 'Gender <input type="text" name="signup_gender"><br>'; ?>
 		<?php echo 'Preference <input type="text" name="signup_preference"><br>'; ?>
+		/*
+		Do we want to give the user to type in Location and Preference?
+		Or should we give them a choice? (Or allow us to track their locations based on the IP Address)
+		Prefernence should come down to :
+			1) Interested in Man
+			2) Interested in Woman
+		*/
     </body>
 </html>

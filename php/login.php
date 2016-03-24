04dad1e8e1cf40b814333660a90a067eba390e05
peterysc <peterysc@gmail.com>
2016-03-24 00:04:03 -0700

    log-in page still implementing

--- php/login.php
+++ php/login.php
@@ -1,14 +1,39 @@
+
 <html>
  	<head>
-  		<title>Welcome to Tinder++</title>
+  		<title>Login to Continue</title>
 	</head>
  	<body>
-		<?php echo '<p>Tinder++ Main Page</p><br>'; ?>
-		<?php echo 'UserID: <input type="text" name="login_name"><br>'; ?>
-		<?php echo 'Password <input type="text" name="login_password"><br>'; ?>
-		
-		<?php echo '<a href="#">Login as User</a>'; ?>
-		<?php echo '<a href="#">Login as Business</a>'; ?>
-		<?php echo '<a href="#">Signup now!</a>'; ?>
+ 		<p>Welcome to Tinder++ </br>
+ 		</br>
+ 			Before you can enjoy our new features, please log in to continue!</p>
+
+ 		<div id="login">
+ 			<p>If you are a user, please log in here.</p>
+ 			<form method="POST" action="login.php">
+ 				<p>
+ 					<label for="login_username">Username:</label>
+ 					<input type="text" name="login_username" size="16"></br>
+ 					<label for="login_userPassword">Password:</label>
+ 					<input type="password" name="login_userPassword" size="16"><br>
+					<input type="submit" value="Continue" name="UserSubmit"></p>
+			</form>
+
+			<p>If you are a business, please log in here.</p>
+
+ 			<form method="POST" action="login.php">
+ 				<p>
+ 					<label for="login_businessname">Business Name:</label>
+ 					<input type="text" name="login_businessname" size="16"></br>
+ 					<label for="login_businessPassword">Password:</label>
+ 					<input type="password" name="login_businessPassword" size="16"><br>
+					<input type="submit" value="Continue" name="BusinessSubmit"></p>
+			</form>
+		</div>
     </body>
 </html>
+
+<?php
+	include 'sql-cmds.php';
+?>
+

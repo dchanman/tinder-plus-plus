75b6c4eef63fe088f7e04dfd0bd8514762dfa159
Emmett Tan <z2p8@annacis.ugrad.cs.ubc.ca>
2016-03-23 17:43:01 -0700

    added more samples, changed them to fit the updated schema

--- php/sql/sample.sql
+++ php/sql/sample.sql
@@ -1,6 +1,11 @@
+INSERT INTO Users VALUES (UserIDSequence.nextval, 'Adam', 'a', '03.26.1997', 'Vancouver', 21, 'm', 1, 0, 'hunter1');
+INSERT INTO Users VALUES (UserIDSequence.nextval, 'Billy', 'b', '03.27.1992', 'Vancouver', 22, 'm', 0, 1, 'hunter2');
+INSERT INTO Users VALUES (UserIDSequence.nextval, 'Carl', 'c', '12.03.1995', 'Texas', 23, 'm', 1, 1, 'hunter3');
+INSERT INTO Users VALUES (UserIDSequence.nextval, 'Donny', 'd', '01.14.1994', 'Vancouver', 19, 'm', 0, 1, 'hunter4');
+INSERT INTO Users VALUES (UserIDSequence.nextval, 'Elliott', 'e', '01.16.1999', 'Vancouver', 31, 'm', 1, 1, 'hunter5');
 
-INSERT INTO Users VALUES (UserIDSequence.nextval, 'Adam', 'a', '03.26.2016', 'Vancouver', 21, 'm', 'f', 'hunter1');
-INSERT INTO Users VALUES (UserIDSequence.nextval, 'Billy', 'b', '03.27.2016', 'Vancouver', 22, 'm', 'm', 'hunter2');
-INSERT INTO Users VALUES (UserIDSequence.nextval, 'Carl', 'c', '12.03.2016', 'Texas', 23, 'm', 'f', 'hunter3');
-INSERT INTO Users VALUES (UserIDSequence.nextval, 'Donny', 'd', '01.14.2016', 'Vancouver', 19, 'm', 'f', 'hunter4');
-INSERT INTO Users VALUES (UserIDSequence.nextval, 'Elliott', 'e', '01.16.2016', 'Vancouver', 31, 'm', 'f', 'hunter5')
+INSERT INTO Users VALUES (UserIDSequence.nextval, 'Gina', 'f', '01.26.1995', 'Vancouver', 21, 'f', 1, 0, 'hunter1');
+INSERT INTO Users VALUES (UserIDSequence.nextval, 'Mary', 'g', '02.27.1994', 'Vancouver', 22, 'f', 0, 1, 'hunter2');
+INSERT INTO Users VALUES (UserIDSequence.nextval, 'Tammy', 'h', '12.03.1996', 'Texas', 23, 'f', 1, 1, 'hunter3');
+INSERT INTO Users VALUES (UserIDSequence.nextval, 'Kara', 'i', '01.14.1994', 'Vancouver', 19, 'f', 0, 1, 'hunter4');
+INSERT INTO Users VALUES (UserIDSequence.nextval, 'Emily', 'j', '01.16.1993', 'Vancouver', 31, 'f', 1, 1, 'hunter5');

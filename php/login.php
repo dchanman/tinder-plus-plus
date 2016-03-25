3a23eab7f0c749e4fec95150d7237d836012b1ac
peterysc <peterysc@gmail.com>
2016-03-24 21:33:22 -0700

    profile works now

--- php/login.php
+++ php/login.php
@@ -6,6 +6,10 @@ if (isset($_POST['UserLogin'])) {
 		$error = "Username or Password is invalid";
 	}
 
+	// else if (empty($_POST['businessName']) || empty($_POST['businessPwd'])) {
+	// 	$error = "Business Name or Password is invalid";
+	// }
+
 	else{
 		include 'sql-cmds.php';
 
@@ -13,56 +17,26 @@ if (isset($_POST['UserLogin'])) {
 
 		if ($db_conn) {
 
-			if(array_key_exists('UserLogin', $_POST)){
+			$inputname = $_POST['userName'];
+			$inputpwd = $_POST['userPwd'];
 
-				$inputname = $_POST['userName'];
-				echo ($inputname."</br>");
-				$inputpwd = $_POST['userPwd'];
-				echo ($inputpwd."</br>");
-
-				$s = executePlainSQL("select count (*)
+			$s = executePlainSQL("select count (*)
 								from Users 
 								where UserName = '$inputname' AND 
 								PasswordHash = '$inputpwd'");
 
-				$result = oci_fetch_array($s);
-
-				if($result[0]==1){
-					echo "Valid username and password";
-					$_SESSION['login_user']=$inputname; // Initializing Session
-					header("location: user_profile.php"); // Redirecting To Other Page
-				}
-				else{
-					echo '<script type="text/javascript">alert("Username or Password is incorrect. Please try Again.");</script>';
-				}
-
-				
+			$result = oci_fetch_array($s);
+			echo($result[0]);
+			
+			if($result[0]==1){
+				$_SESSION['login_user']=$inputname; // Initializing Session
+				header("location: user_profile.php"); // Redirecting To Other Page
 			}
-
-			else if(array_key_exists('BusinessLogin', $_POST)){
-
-				$inputname = $_POST['businessName'];
-				echo ($inputname."</br>");
-				$inputpwd = $_POST['businessPwd'];
-				echo ($inputpwd."</br>");
-
-				$s = executePlainSQL("select count (*)
-								from Business 
-								where BusinessName = '$inputname' AND 
-								PasswordHash = '$inputpwd'");
-
-				$result = oci_fetch_array($s);
-
-				if($result[0]==1){
-					echo "Valid Business name and password";
-					$_SESSION['login_business']=$inputname; // Initializing Session
-					header("location: business_profile.php"); // Redirecting To Other Page
-				}
-				else{
-					echo '<script type="text/javascript">alert("Business Name or Password is incorrect. Please try Again.");</script>';
-				}
+			else{
+				$error = "Username or Password is invalid";
 			}
-			OCILogoff($db_conn);
+			oci_commit($db_conn);
+			oci_close($db_conn);
 		}
 		else{
 			echo "cannot connect";

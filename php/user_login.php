911393d50c77c50203e275362fb9e8e12ac53034
Derek Chan <derekchan1994@gmail.com>
2016-04-02 18:33:41 -0700

    playing with divs on login

--- php/user_login.php
+++ php/user_login.php
@@ -22,19 +22,22 @@ include 'verify.php';
  			Before you can enjoy our new features, please log in to continue</p>
 
  		<div id="login">
- 			<form method="POST" action="user_login.php">
- 				<p>
- 					<label for="username">Username:</label>
- 					<input type="text" name="userName" placeholder="username" size="16"></br>
- 					<label for="userPwd">Password:</label>
- 					<input type="password" name="userPwd" placeholder="**********" size="16"><br>
-					<input id="UserLogin" type="submit" value="Login" name="UserLogin"></p>
+ 			<form method="POST" action="user_login.php" class="form-inline">
+
+ 					<div class="col-xs-3 form-group">
+ 					<input type="text" class="form-control col-xs-3" name="userName" placeholder="Username">
+  					<input type="password" class="form-control" name="userPwd" placeholder="Password">
+ 					<input id="UserLogin" class="btn btn-default" type="submit" value="Login" name="UserLogin"></p>
+ 					</div>
 					<span><?php echo $error; ?></span>
 			</form>
+
+			<div id="userSignup">
+				<p>Tinder++ is free and always will be!</p>
+				<input type="button" class="btn btn-default" value="Sign up" onclick="UserSignup();"></p>
+			</div>
 		</div>
 
-		<div id="userSignup">
-			<input type="button" value="Sign up for free" onclick="UserSignup();"></p>
-		</div>
+		
     </body>
 </html>

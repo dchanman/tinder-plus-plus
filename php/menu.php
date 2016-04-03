bdaf4e6c99715bcb042be3a912b53412d60b7ca4
Derek Chan <derekchan1994@gmail.com>
2016-04-02 19:26:50 -0700

    Reorganized menu. Updated Bootstrap versions.

--- php/menu.php
+++ php/menu.php
@@ -1,15 +1,40 @@
-<ul class = "nav nav-tabs">
-	<li role="presentation"><a href='index.php'>Home</a></li>
-	<li role="presentation"><a href='newuser.php'>User Signup</a></li>
-	<li role="presentation"><a href='user_login.php'>User Login</a></li>
-	<li role="presentation"><a href='user_profile.php'>User Profile</a></li>
-	<li role="presentation"><a href='match.php'>Find Matches</a></li>
-	<li role="presentation"><a href='test-messaging.php'>Messages</a></li>
-	<li role="presentation"><a href='newbusiness.php'>Business Signup</a></li>
-	<li role="presentation"><a href='business_login.php'>Business Login</a></li>
-	<li role="presentation"><a href='business_profile.php'>Business Profile</a></li>
-	<li role="presentation"><a href='logout.php'>Log Out</a></li>
-	<li role="presentation"><a href='tinder-test.php'>Database Tables</a></li>
-</ul>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script src="dist/js/bootstrap.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<ul class = "nav nav-tabs">
+
+	<li role="presentation"><a href='index.php'>Home</a></li>
+
+	<li class="dropdown">
+    <a class="dropdown-toggle" data-toggle="dropdown" href="#">User Features
+    <span class="caret"></span></a>
+    <ul class="dropdown-menu">
+		<li role="presentation"><a href='newuser.php'>User Signup</a></li>
+		<li role="presentation"><a href='user_login.php'>User Login</a></li>
+		<li role="presentation"><a href='user_profile.php'>User Profile</a></li>
+		<li role="presentation"><a href='match.php'>Find Matches</a></li>
+		<li role="presentation"><a href='test-messaging.php'>Messages</a></li>
+		<li role="presentation"><a href='logout.php'>Log Out</a></li>
+    </ul>
+  	</li>
+
+  	<li class="dropdown">
+    <a class="dropdown-toggle" data-toggle="dropdown" href="#">Business Features
+    <span class="caret"></span></a>
+    <ul class="dropdown-menu">
+		<li role="presentation"><a href='newbusiness.php'>Business Signup</a></li>
+		<li role="presentation"><a href='business_login.php'>Business Login</a></li>
+		<li role="presentation"><a href='business_profile.php'>Business Profile</a></li>
+		<li role="presentation"><a href='logout.php'>Log Out</a></li>
+    </ul>
+  	</li>
+
+  	<li class="dropdown">
+    <a class="dropdown-toggle" data-toggle="dropdown" href="#">Utils
+    <span class="caret"></span></a>
+    <ul class="dropdown-menu">
+		<li role="presentation"><a href='tinder-test.php'>Database Tables</a></li>
+		<li role="presentation"><a href='tinder-test.php'>CS304 Student Info</a></li>
+    </ul>
+  	</li>
+</ul>
+

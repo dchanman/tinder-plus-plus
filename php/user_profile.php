3b9e9142c47dfb18b972836e731709f23ebf91a0
Emmett Tan <z2p8@annacis.ugrad.cs.ubc.ca>
2016-04-03 00:11:09 -0700

    styled user_profile.php

--- php/user_profile.php
+++ php/user_profile.php
@@ -11,41 +11,47 @@ $name = $_SESSION['login_user'];
 <html id='ngWrapper' ng-app='user_profile_module' ng-controller='user_profile_controller'>
 	<head>
 		<title>Dashboard</title>
-		<script src='js/ang_user_profile.js' type='text/javascript'></script>
 	</head>
 	<body>
 		<?php
 		include 'head-includes.php';
 		include 'menu.php';
-
-		echo "<h2>$user_username's profile</h2>";
-		echo "<b>$user_name, $user_age$user_gender</b><br>";
-		echo "<b>$user_location</b><br>";
-		echo "Seeking: <b><i> $user_interest</b></i><br>";
 		?>
+		<div class="maincontent">
+			<div class="container">
+				<div class="col-xs-offset-4 col-xs-4 jumbotron">
+					<h2 class="center-text"><?php echo $user_name; ?></h2>
+					<h3 class="center-text"><?php echo $user_location; ?></h3><br>
+					<p>Age: <?php echo $user_age; ?></p>
+					<p>Gender: <?php echo $user_gender; ?></p>
+					<p>Seeking: <?php echo $user_interest; ?></p>
+				</div>
+			</div>
 
-		<?php
-		/* Print interests */
-		echo "<h3><b>Your Interests</b></h3><ul>";
-		$userInterests = query_getUserInterests($user_userid);
-		foreach ($userInterests as $interest) {
-			echo "<li>$interest</i>";
-		}
-		echo "</ul>";
-
-		/* Print photos */
-		echo "<h3><b>Your Photos</b></h3><ul>";
-		$result = query_images($user_userid);
-		foreach ($result as $img) {
-      		echo "<p><img src=\"" . $img . "\" width=150></img></p>";
-      	}
-      	echo "</ul>";
-		?>
+			<div class="container">
+				<div class="col-xs-offset-4 col-xs-4 jumbotron">
+					<?php
+					/* Print interests */
+					echo "<h3><b>Your Interests</b></h3><ul>";
+					$userInterests = query_getUserInterests($user_userid);
+					foreach ($userInterests as $interest) {
+						echo "<li>$interest</i>";
+					}
+					echo "</ul>";
+						/* Print photos */
+					echo "<h3><b>Your Photos</b></h3><ul>";
+					$result = query_images($user_userid);
+					foreach ($result as $img) {
+						echo "<p><img style='border-radius: 45px;' src=\"" . $img . "\" width=150></img></p>";
+					}
+					echo "</ul>";
+					?>
+				</div>
+			</div>
+		</div>
 		<?php 
-		include 'footer_menu.php';
+			include 'footer_menu.php';
 		?>
-
-
 		</body>
 </html>
 
